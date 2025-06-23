698eda83984e3c2d30585892e18dccccb8e26e85
Thales Maia <thalesmaiaalexandre@gmail.com>
2025-06-23 20:18:21 -0300

    fix: add @Min validation for servingSize in CreateRecipeRequest and UpdateRecipeRequest

--- src/main/java/com/recipes/adapter/in/dto/request/CreateRecipeRequest.java
+++ src/main/java/com/recipes/adapter/in/dto/request/CreateRecipeRequest.java
@@ -1,17 +1,14 @@
 package com.recipes.adapter.in.dto.request;
 
 import com.recipes.core.domain.Recipe;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.List;
 
 public record CreateRecipeRequest(@NotBlank @Size(max = 255) String title,
                                   @Size(max = 255) @NotBlank String description,
                                   @NotEmpty List<String> ingredients, @NotEmpty List<String> instructions,
-                                  boolean vegetarian, @NotNull Integer servingSize) {
+                                  boolean vegetarian, @NotNull @Min(1) Integer servingSize) {
 
     public Recipe toDomain() {
         return new Recipe(title, description, ingredients, instructions, vegetarian, servingSize);
--- src/main/java/com/recipes/adapter/in/dto/request/UpdateRecipeRequest.java
+++ src/main/java/com/recipes/adapter/in/dto/request/UpdateRecipeRequest.java
@@ -1,13 +1,14 @@
 package com.recipes.adapter.in.dto.request;
 
 import com.recipes.core.domain.Recipe;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record UpdateRecipeRequest(@Size(max = 255) String title, @Size(max = 255) String description,
                                   List<String> ingredients, List<String> instructions, Boolean vegetarian,
-                                  Integer servingSize) {
+                                  @Min(1) Integer servingSize) {
 
     public Recipe toDomain(Long id) {
         return new Recipe(id, title, description, ingredients, instructions, vegetarian, servingSize);
