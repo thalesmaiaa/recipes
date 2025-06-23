aceab780b094d5717f1536d0b877c7095fadbe15
Thales Maia <thalesmaiaalexandre@gmail.com>
2025-06-23 20:12:27 -0300

    add size validation for title and description in CreateRecipeRequest and UpdateRecipeRequest

--- src/main/java/com/recipes/adapter/in/dto/request/CreateRecipeRequest.java
+++ src/main/java/com/recipes/adapter/in/dto/request/CreateRecipeRequest.java
@@ -4,10 +4,12 @@ import com.recipes.core.domain.Recipe;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
-public record CreateRecipeRequest(@NotBlank String title, @NotBlank String description,
+public record CreateRecipeRequest(@NotBlank @Size(max = 255) String title,
+                                  @Size(max = 255) @NotBlank String description,
                                   @NotEmpty List<String> ingredients, @NotEmpty List<String> instructions,
                                   boolean vegetarian, @NotNull Integer servingSize) {
 
--- src/main/java/com/recipes/adapter/in/dto/request/UpdateRecipeRequest.java
+++ src/main/java/com/recipes/adapter/in/dto/request/UpdateRecipeRequest.java
@@ -1,11 +1,13 @@
 package com.recipes.adapter.in.dto.request;
 
 import com.recipes.core.domain.Recipe;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
-public record UpdateRecipeRequest(String title, String description, List<String> ingredients,
-                                  List<String> instructions, Boolean vegetarian, Integer servingSize) {
+public record UpdateRecipeRequest(@Size(max = 255) String title, @Size(max = 255) String description,
+                                  List<String> ingredients, List<String> instructions, Boolean vegetarian,
+                                  Integer servingSize) {
 
     public Recipe toDomain(Long id) {
         return new Recipe(id, title, description, ingredients, instructions, vegetarian, servingSize);
